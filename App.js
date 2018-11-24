10e4484d7f3fa79e2b36973a2fa52ee2e03f0790
Daniel Kiesshau <danielkiesshau@outlook.com>
2018-11-24 15:20:59 -0200

    Drag & Drop with decay red box

--- App.js
+++ App.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Animated, ScrollView } from "react-native";
+import { StyleSheet, View, Animated, PanResponder } from "react-native";
 
 export default class animations extends Component {
   state = {
-    animation: new Animated.Value(0),
+    animation: new Animated.ValueXY(0),
+  }
+
+  componentWillMount() {
+
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.animation.x,
+          dy: this.state.animation.y,
+        }
+      ]),
+      onPanResponderRelease:  (e, { vx, vy }) => {
+        Animated.decay(this.state.animation, {
+          velocity: { x: vx, y: vy },
+        }).start();
+      },
+      onPanResponderGrant: (e, gestureEvent) => {
+        this.state.animation.extractOffset()
+      }
+    });
   }
   render() {
-
-    const backgroundInterpolate = this.state.animation.interpolate({
-      inputRange: [0, 3000],
-      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
-    });
-
-    const backgroundStyle = {
-      backgroundColor: backgroundInterpolate
+    const animatedStyle = {
+      transform: this.state.animation.getTranslateTransform()
     }
+
     return (
       <View style={styles.container}>
-        <ScrollView
-          scrollEventThrottle={16}
-          onScroll={Animated.event([
-            {
-              nativeEvent: {
-                contentOffset: {
-                  y: this.state.animation
-                },
-              }
-            }
-          ])}
-        >
-          <Animated.View style={[styles.content, backgroundStyle]} />
-        </ScrollView>
+          <Animated.View
+            style={[styles.content, animatedStyle]}
+            { ...this._panResponder.panHandlers }
+          />
       </View>
     );
 
@@ -40,8 +48,12 @@ export default class animations extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   content: {
-    height: 3000,
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
   }
 });
