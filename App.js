af7d7ba5ae40fc608ad13a9934a5b8b0cd2d88ea
Daniel Kiesshau <danielkiesshau@outlook.com>
2018-11-24 14:52:32 -0200

    Animated Event

--- App.js
+++ App.js
@@ -1,64 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View, Animated, Easing, TouchableWithoutFeedback } from 'react-native';
- 
-export default class App extends React.Component {
+import React, { Component } from "react";
+import { StyleSheet, View, Animated, ScrollView } from "react-native";
+
+export default class animations extends Component {
   state = {
     animation: new Animated.Value(0),
-  };
-
-  startAnimation = () => {
-    this.state.animation.addListener(({ value }) => this._value = value);
-    const toValue = 115;
-
-    if (this.state.animation._value === 0) {
-      Animated.loop(this.state.animation, {
-        toValue,
-        duration: 650,
-        tension: 2,
-        friction: 450,
-        // easing: Easing.back(15),
-        // easing: Easing.bounce
-        // easing: Easing.elastic(3)
-        // easing: Easing.bezier(.06,1,.86,.23)
-      }).start();
-    } else {
-      Animated.loop(this.state.animation, {
-        toValue: 0,
-        duration: 650,
-        tension: 0,
-        friction: 160,
-        // easing: Easing.back(15),
-        // easing: Easing.bounce
-        // easing: Easing.elastic(3)
-        // easing: Easing.bezier(.06,1,.86,.23)
-      }).start();
-    }
-    
-  };
-
+  }
   render() {
-    const animatedStyles = {
-      transform: [{ translateY: this.state.animation }],
-      backgroundColor: this.state.animation.interpolate({ inputRange: [0, 85], outputRange: ['rgb(255, 0, 0)', 'rgb(0, 255, 0)']})
-    };
+
+    const backgroundInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 3000],
+      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
+    });
+
+    const backgroundStyle = {
+      backgroundColor: backgroundInterpolate
+    }
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, animatedStyles]} />
-        </TouchableWithoutFeedback>
+        <ScrollView
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  y: this.state.animation
+                },
+              }
+            }
+          ])}
+        >
+          <Animated.View style={[styles.content, backgroundStyle]} />
+        </ScrollView>
       </View>
     );
+
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
   },
-  box: {
-    width: 150,
-    height: 150,
-    backgroundColor: "tomato",
-  },
+  content: {
+    height: 3000,
+  }
 });
